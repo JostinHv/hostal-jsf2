2e442b7dc78e3f188bd4492aa14fceffbe2fdf47
JostinHv <jostin102012@hotmail.com>
2024-02-22 23:38:38 -0500

    login

--- src/main/java/presentation/PresentadorLogin.java
+++ src/main/java/presentation/PresentadorLogin.java
@@ -1,35 +1,39 @@
 package presentation;
 
-import java.io.Serializable;
+import aplication.servicios.LoginService;
+import domain.entity.Empleado;
+import lombok.Getter;
+import lombok.Setter;
 
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
-import domain.entity.Usuario;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
 
 @ManagedBean
 @SessionScoped
-public class PresentadorLogin implements Serializable {
+public class PresentadorLogin  implements Serializable {
 
-	private static final long serialVersionUID = -7590257808781595903L;
-
-	private String cuenta;
+	private LoginService loginService;
+	@Getter
+	@Setter
+	private Empleado empleadoAutenticado;
 
 	public PresentadorLogin() {
-
+		this.loginService = new LoginService();
+		this.empleadoAutenticado= new Empleado();
 	}
 
-	public String getCuenta() {
-		return "hola";
-	}
-
-	public void setCuenta(String cuenta) {
-		this.cuenta = cuenta;
-	}
-
-	public String autenticarse() {
-
-		return null;
+	public String login() {
+		empleadoAutenticado = loginService.autenticar(empleadoAutenticado.getUsername(), empleadoAutenticado.getPassword());
+		if (empleadoAutenticado != null) {
+			return "Home?faces-redirect=true";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Invalid credentials"));
+			return null;
+		}
 	}
 
 }
